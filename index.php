9eaf80d16b007168c6486a045897cacca87f6ef4
Daniel Sinn <daniel.p.sinn@gmail.com>
2019-01-05 15:08:28 -0500

    "Move" legacy site and prep for new site

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Serious Sam Done Quick</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.3/slate/bootstrap.min.css" />
+        <link rel="stylesheet" href="bfe.css" />
+    </head>
+    <body>
+        <main class="container">
+            <h1 class="mt-5"><img height="48" src="logo.svg" alt /> Serious&nbsp;Sam Done&nbsp;Quick</h1>
+            <p class="lead">
+                We're currently under construction, but in the meantime, here are some links that you may find useful:
+            </p>
+            <div class="list-group list-group-flush mb-5">
+                <a class="list-group-item list-group-item-action" href="https://www.speedrun.com/serious_sam" target="_blank"><img height="32" src="speedrun_com.png" alt="speedrun.com" /></a>
+                <a class="list-group-item list-group-item-action" href="https://discord.gg/0vKwJa1xQoCpw7vY" target="_blank"><img height="32" src="discord.svg" alt="Discord" /></a>
+            </div>
+
+            <p>For the old site that was maintained by TheVoiid, click <a href="legacy.html">here</a>.</p>
+
+            <h2>Contact</h2>
+            <p>Please contact <em>mr.deagle#6969</em> on our <a href="https://discord.gg/0vKwJa1xQoCpw7vY" target="_blank">Discord server</a>.</p>
+        </main>
+    </body>
+</html>
