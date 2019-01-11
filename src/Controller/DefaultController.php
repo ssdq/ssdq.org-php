a5d1948ff0b986d85984d867e3cdb01c8c43f142
Daniel Sinn <daniel.p.sinn@gmail.com>
2019-01-11 02:24:08 -0500

    Use dependency injection for the API
    Closes #3.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,25 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class DefaultController extends AbstractController
 {
+    /** @var APIHelper */
+    private $srcomAPIHelper;
+
+    public function __construct(APIHelper $srcomAPIHelper)
+    {
+        $this->srcomAPIHelper = $srcomAPIHelper;
+    }
+
     public function index()
     {
-        // @TODO Dependency injection
-        $stack = \GuzzleHttp\HandlerStack::create();
-        $stack->push(new \Kevinrob\GuzzleCache\CacheMiddleware(
-            new \Kevinrob\GuzzleCache\Strategy\GreedyCacheStrategy(
-                new \Kevinrob\GuzzleCache\Storage\DoctrineCacheStorage(
-                    new \Doctrine\Common\Cache\FilesystemCache(sys_get_temp_dir())
-                ),
-                1800
-            )
-        ), 'greedy-cache');
-        $httpClient = new \GuzzleHttp\Client(['base_uri' => \Dsinn\SrcomApi\Client::DEFAULT_BASE_URI, 'handler' => $stack]);
-        $apiClient = new \Dsinn\SrcomApi\Client($httpClient);
-        $apiHelper = new APIHelper($apiClient);
-
         return $this->render('index.html.twig', [
-            'latestRuns' => $apiHelper->getLatestRuns(),
-            'worldRecords' => $apiHelper->getWorldRecords(),
+            'latestRuns' => $this->srcomAPIHelper->getLatestRuns(),
+            'worldRecords' => $this->srcomAPIHelper->getWorldRecords(),
         ]);
     }
 }
