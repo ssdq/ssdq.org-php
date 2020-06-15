6a2cc83ef16e4f75e41d006be407b13390b3d653
Daniel Sinn <dsinn@pagerduty.com>
2020-06-15 07:18:03 -0400

    A mildly more graceful way to handle API request failures

--- public_html/home.js
+++ public_html/home.js
@@ -2,7 +2,9 @@
     ['latest_runs', 'world_records'].forEach(table => {
         const oReq = new XMLHttpRequest();
         oReq.addEventListener('load', function () {
-            document.getElementById(table).innerHTML = this.responseText;
+            document.getElementById(table).innerHTML = oReq.status === 200
+                ? this.responseText
+                : 'An error has occurred while attempting to load data from speedrun.com';
         });
         oReq.open('GET', `/${table}`);
         oReq.send();
